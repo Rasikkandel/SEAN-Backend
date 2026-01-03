260357043ae6c4207e42f1e63daa50d02edfd729
Rashik Kandel <kandelrasik123@gmail.com>
2026-01-03 16:24:36 +0545

    added cors headers

--- SEAN/settings.py
+++ SEAN/settings.py
@@ -42,11 +42,13 @@ INSTALLED_APPS = [
     'resources', 
     'projects', 
     'rest_framework' , 
+    'corsheaders' , 
 ]    
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
+    'corsheaders.middleware.CorsMiddleware', 
     'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
     'django.contrib.auth.middleware.AuthenticationMiddleware',
@@ -125,3 +127,9 @@ AUTH_USER_MODEL = 'USer.CustomUser'
 
 MEDIA_URL = '/media/'
 MEDIA_ROOT = BASE_DIR / 'media' 
+
+REST_FRAMEWORK = {
+'DEFAULT_PERMISSION_CLASSES': [
+'rest_framework.permissions.AllowAny',
+]
+}   
