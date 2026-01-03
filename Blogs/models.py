113095133c7bcfbaa486188cd166fbc5f73a69be
Rashik Kandel <kandelrasik123@gmail.com>
2026-01-03 12:28:44 +0545

    Added serializers

--- Blogs/models.py
+++ Blogs/models.py
@@ -2,7 +2,7 @@ from django.db import models
 from django.conf import settings
 
 class Blog(models.Model):
-    name = models.CharField(max_length=200)  
+    title = models.CharField(max_length=200)  
     description = models.CharField(max_length=500) 
     body = models.TextField()  
     created_at = models.DateTimeField(auto_now_add=True)
